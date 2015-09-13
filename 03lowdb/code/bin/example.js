52ef96f888df44929d7ba1d2f9c68f37d86a9d8f
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2015-09-13 14:01:02 +0200

    added lowdb example

---
+++ 03lowdb/code/bin/example.js
@@ -0,0 +1,97 @@
+(function (console) { "use strict";
+var Main = function() {
+	console.log("Node.js LowDB Example");
+	var str = new Date();
+	var _lowdb = (require("lowdb"))("db.json",null);
+	_lowdb("posts").push({ title : "lowdb is awesome", date : str});
+	console.log(_lowdb("posts").find({ title : "lowdb is awesome"}));
+	console.log("_lowdb.object: " + Std.string(_lowdb.object));
+};
+Main.__name__ = true;
+Main.main = function() {
+	var main = new Main();
+};
+Math.__name__ = true;
+var Std = function() { };
+Std.__name__ = true;
+Std.string = function(s) {
+	return js_Boot.__string_rec(s,"");
+};
+var js_Boot = function() { };
+js_Boot.__name__ = true;
+js_Boot.__string_rec = function(o,s) {
+	if(o == null) return "null";
+	if(s.length >= 5) return "<...>";
+	var t = typeof(o);
+	if(t == "function" && (o.__name__ || o.__ename__)) t = "object";
+	switch(t) {
+	case "object":
+		if(o instanceof Array) {
+			if(o.__enum__) {
+				if(o.length == 2) return o[0];
+				var str2 = o[0] + "(";
+				s += "\t";
+				var _g1 = 2;
+				var _g = o.length;
+				while(_g1 < _g) {
+					var i1 = _g1++;
+					if(i1 != 2) str2 += "," + js_Boot.__string_rec(o[i1],s); else str2 += js_Boot.__string_rec(o[i1],s);
+				}
+				return str2 + ")";
+			}
+			var l = o.length;
+			var i;
+			var str1 = "[";
+			s += "\t";
+			var _g2 = 0;
+			while(_g2 < l) {
+				var i2 = _g2++;
+				str1 += (i2 > 0?",":"") + js_Boot.__string_rec(o[i2],s);
+			}
+			str1 += "]";
+			return str1;
+		}
+		var tostr;
+		try {
+			tostr = o.toString;
+		} catch( e ) {
+			return "???";
+		}
+		if(tostr != null && tostr != Object.toString && typeof(tostr) == "function") {
+			var s2 = o.toString();
+			if(s2 != "[object Object]") return s2;
+		}
+		var k = null;
+		var str = "{\n";
+		s += "\t";
+		var hasp = o.hasOwnProperty != null;
+		for( var k in o ) {
+		if(hasp && !o.hasOwnProperty(k)) {
+			continue;
+		}
+		if(k == "prototype" || k == "__class__" || k == "__super__" || k == "__interfaces__" || k == "__properties__") {
+			continue;
+		}
+		if(str.length != 2) str += ", \n";
+		str += s + k + " : " + js_Boot.__string_rec(o[k],s);
+		}
+		s = s.substring(1);
+		str += "\n" + s + "}";
+		return str;
+	case "function":
+		return "<function>";
+	case "string":
+		return o;
+	default:
+		return String(o);
+	}
+};
+String.__name__ = true;
+Array.__name__ = true;
+Date.__name__ = ["Date"];
+var EventEmitter__1 = require("events").EventEmitter;
+var Readable__2 = require("stream").Readable;
+var Writable__3 = require("stream").Writable;
+var LowDb__0 = require("lowdb").LowDb;
+Main.main();
+})(typeof console != "undefined" ? console : {log:function(){}});
