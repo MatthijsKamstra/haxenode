991c3514593a691555281508055a06f9f9c31442
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2015-09-13 11:15:01 +0200

    added an writing example

---
+++ 01writing/about.md
@@ -0,0 +1,13 @@
+#About
+
+>Writing to a file is another of the basic programming tasks that one usually needs to know about - luckily, this task is very simple in Node.js. We can use the handy writeFile method inside the standard library's fs module, which can save all sorts of time and trouble.
+
+*source: [how-to-write-files-in-nodejs](https://docs.nodejitsu.com/articles/file-system/how-to-write-files-in-nodejs)*
+
+
+>Asynchronously writes data to a file, replacing the file if it already exists. data can be a string or a buffer.
+>
+>The encoding option is ignored if data is a buffer. It defaults to 'utf8'.
+
+Read more about it in the node.js api:
+[https://nodejs.org/api/fs.html]()
---
+++ 01writing/code/bin/example.js
@@ -0,0 +1,115 @@
+(function (console) { "use strict";
+var HxOverrides = function() { };
+HxOverrides.__name__ = true;
+HxOverrides.dateStr = function(date) {
+	var m = date.getMonth() + 1;
+	var d = date.getDate();
+	var h = date.getHours();
+	var mi = date.getMinutes();
+	var s = date.getSeconds();
+	return date.getFullYear() + "-" + (m < 10?"0" + m:"" + m) + "-" + (d < 10?"0" + d:"" + d) + " " + (h < 10?"0" + h:"" + h) + ":" + (mi < 10?"0" + mi:"" + mi) + ":" + (s < 10?"0" + s:"" + s);
+};
+var Main = function() {
+	console.log("Node.js writing Example");
+	var str;
+	str = "Hello World!\nWritten on: " + (function($this) {
+		var $r;
+		var _this = new Date();
+		$r = HxOverrides.dateStr(_this);
+		return $r;
+	}(this));
+	(Fs__0||require("fs")).writeFile("helloworld.txt",str);
+	(Fs__0||require("fs")).writeFile("hello.txt",str,{ },function(err) {
+		if(err != null) console.log("err: " + Std.string(err)); else console.log("Hello > hello.txt");
+	});
+};
+Main.__name__ = true;
+Main.main = function() {
+	var main = new Main();
+};
+Math.__name__ = true;
+var Std = function() { };
+Std.__name__ = true;
+Std.string = function(s) {
+	return js_Boot.__string_rec(s,"");
+};
+var js_Boot = function() { };
+js_Boot.__name__ = true;
+js_Boot.__string_rec = function(o,s) {
+	if(o == null) return "null";
+	if(s.length >= 5) return "<...>";
+	var t = typeof(o);
+	if(t == "function" && (o.__name__ || o.__ename__)) t = "object";
+	switch(t) {
+	case "object":
+		if(o instanceof Array) {
+			if(o.__enum__) {
+				if(o.length == 2) return o[0];
+				var str2 = o[0] + "(";
+				s += "\t";
+				var _g1 = 2;
+				var _g = o.length;
+				while(_g1 < _g) {
+					var i1 = _g1++;
+					if(i1 != 2) str2 += "," + js_Boot.__string_rec(o[i1],s); else str2 += js_Boot.__string_rec(o[i1],s);
+				}
+				return str2 + ")";
+			}
+			var l = o.length;
+			var i;
+			var str1 = "[";
+			s += "\t";
+			var _g2 = 0;
+			while(_g2 < l) {
+				var i2 = _g2++;
+				str1 += (i2 > 0?",":"") + js_Boot.__string_rec(o[i2],s);
+			}
+			str1 += "]";
+			return str1;
+		}
+		var tostr;
+		try {
+			tostr = o.toString;
+		} catch( e ) {
+			return "???";
+		}
+		if(tostr != null && tostr != Object.toString && typeof(tostr) == "function") {
+			var s2 = o.toString();
+			if(s2 != "[object Object]") return s2;
+		}
+		var k = null;
+		var str = "{\n";
+		s += "\t";
+		var hasp = o.hasOwnProperty != null;
+		for( var k in o ) {
+		if(hasp && !o.hasOwnProperty(k)) {
+			continue;
+		}
+		if(k == "prototype" || k == "__class__" || k == "__super__" || k == "__interfaces__" || k == "__properties__") {
+			continue;
+		}
+		if(str.length != 2) str += ", \n";
+		str += s + k + " : " + js_Boot.__string_rec(o[k],s);
+		}
+		s = s.substring(1);
+		str += "\n" + s + "}";
+		return str;
+	case "function":
+		return "<function>";
+	case "string":
+		return o;
+	default:
+		return String(o);
+	}
+};
+String.__name__ = true;
+Array.__name__ = true;
+Date.__name__ = ["Date"];
+var Fs__0 = require("fs");
+var EventEmitter__1 = require("events").EventEmitter;
+var FSWatcher__5 = require("fs").FSWatcher;
+var Stats__4 = require("fs").Stats;
+var Readable__2 = require("stream").Readable;
+var Writable__3 = require("stream").Writable;
+Main.main();
+})(typeof console != "undefined" ? console : {log:function(){}});
