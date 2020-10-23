17b22d1ef0b7ff9857c9613a7284a8606f2449e2
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-10-23 08:51:48 +0200

    updated lowdb dts2hx version, wip

--- 03lowdb/code/src/Main.hx
+++ 03lowdb/code/src/Main.hx
@@ -1,26 +1,44 @@
 package;
 
-// import js.Node;
-// import js.npm.LowDb;
-
-typedef LowDB = Lowdb;
+import js.npm.FileSync;
+import js.html.FileReaderSync;
+import js.Syntax;
+import Lowdb;
 
 /**
  * @author Matthijs Kamstra aka [mck]
  */
 class Main {
 	function new() {
-		trace("Node.js LowDB Example");
+		trace("Node.js Lowdb Example");
 
-		var str = Date.now();
+		// var str = Date.now();
 
-		var _lowdb = LowDb.construct('db.json');
-		LowDb.query(_lowdb, 'posts').push({title: 'lowdb is awesome', date: str});
+		// var _lowdb = Lowdb.call('db.json');
+		// Lowdb.query(_lowdb, 'posts').push({title: 'Lowdb is awesome', date: str});
 
-		trace(LowDb.query(_lowdb, 'posts').find({title: 'lowdb is awesome'}));
+		// trace(Lowdb.query(_lowdb, 'posts').find({title: 'Lowdb is awesome'}));
 
-		// _lowdb.save();
-		trace("_lowdb.object: " + _lowdb.object);
+		// // _lowdb.save();
+		// trace("_lowdb.object: " + _lowdb.object);
+
+		var adapter = new FileSync('db.json');
+		var db = Lowdb.call(adapter);
+
+		trace(Type.typeof(db));
+		trace(db);
+
+		// Set some defaults (required if your JSON file is empty)
+		untyped db.defaults({posts: [], user: {}, count: 0}).write();
+
+		// Add a post
+		untyped db.get('posts').push({id: 1, title: 'lowdb is awesome'}).write();
+
+		// Set a user using Lodash shorthand syntax
+		untyped db.set('user.name', 'typicode').write();
+
+		// Increment count
+		untyped db.update('count', n -> n + 1).write();
 	}
 
 	static public function main() {
